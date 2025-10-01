a45d11da150ed802b04693c0b60bca02368b556c
Leo.G.Changes <gargileo38@gmail.com>
2025-10-01 19:51:29 -0300

    Create Ejercicio 2 IF.java

---
+++ Ejercicio 2 IF.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entero: ");
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número " + numero + " es PAR.");
+        } else {
+            System.out.println("El número " + numero + " es IMPAR.");
+        }
+
+        sc.close();
+    }
+}
